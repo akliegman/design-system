996ab06f1278bcf0f7ea5deab4acad45c63145ec
adam kliegman <kliegman14@gmail.com>
2024-07-10 19:56:27 -0400

    feat: add cache clear to package config.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,10 @@
 import { Heading } from "@/components/Heading";
-import { Icon } from "@/components/Icon";
 import { Link } from "@/components/Link";
 import { Button } from "@/components/buttons/Button";
 import { IconButton } from "@/components/buttons/IconButton";
 import { LinkButton } from "@/components/buttons/LinkButton";
 import { LinkIconButton } from "@/components/buttons/LinkIconButton";
+
+import { Icon } from "./Icon";
 
 export { Heading, Icon, Link, Button, IconButton, LinkButton, LinkIconButton };
