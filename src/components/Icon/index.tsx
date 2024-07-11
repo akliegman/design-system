ae5aba15ec0497d33ee64e2020eabdd1215d9697
adam kliegman <kliegman14@gmail.com>
2024-07-10 20:05:04 -0400

    chore: ensure git recongizes case change of icon directory.

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import clsx from "clsx";
+import { IconType } from "react-icons";
+import { FaBeer } from "react-icons/fa";
+
+import { IconLevels, Variants } from "@/global";
+import { useTheme } from "@/providers/theme";
+
+import styles from "@/components/Icon/styles.module.scss";
+
+export interface IconProps {
+  className?: string;
+  size?: keyof IconLevels;
+  variant?: keyof Variants;
+  icon: IconType;
+}
+
+export const Icon = (props: IconProps) => {
+  const {
+    className,
+    size = "medium",
+    variant = "none",
+    icon: ReactIcon = FaBeer,
+  } = props;
+
+  const { themeIsMounted } = useTheme();
+
+  const rootClassNames = clsx(
+    styles[`icon`],
+    !themeIsMounted && styles[`loading`],
+    size && styles[`level-${size}`],
+    variant && styles[`variant-${variant}`],
+    className,
+  );
+
+  return (
+    <span className={rootClassNames}>
+      <ReactIcon />
+    </span>
+  );
+};
